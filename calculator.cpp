2efbdd9f7a4afb2dabc2761d797dd687664c54df
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    execute cherr-pick command with adding custom line of code
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,17 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+//This is my own part of code
+//added to calculate.cpp file.
+//It was done after executing
+//cherry-pick git command
+int Calculate::Div (double a, double b)
+{
+	return a / b + 0.5;
+}
